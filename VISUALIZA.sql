ad58a94dd5480d3916399d0904499ef2594b9bca
Mario Andre <mario_andre.l.a@hotmail.com>
2020-06-16 16:06:51 -0300

    Projeto finalizado

---
+++ VISUALIZA.sql
@@ -0,0 +1,29 @@
+-- comandos básicos para visualizar os conteudos e formatos das tabelas
+
+desc  horario;
+desc  semestre;
+desc  ministradoresdasdisciplinas;
+desc  professor;
+desc  pedidodematricula;
+desc  disciplinascursadas;
+desc  matricula;
+desc  curriculo;
+desc  curso;
+desc  prerequisito;
+desc  disciplina;
+desc  aluno;
+desc  campus;
+
+select * from  horario;
+select * from  semestre;
+select * from ministradoresdasdisciplinas;
+select * from professor;
+select * from pedidodematricula;
+select * from disciplinascursadas;
+select * from matricula;
+select * from curriculo;
+select * from curso;
+select * from prerequisito;
+select * from disciplina;
+select * from aluno;
+select * from campus;
